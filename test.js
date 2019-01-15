d7ffec97ea169a7967c82be8e6f3f93cc94f0977
MAGNOLIA-RIVER\Danny.Britt <danny.britt@magnolia-river.com>
2019-01-15 08:42:18 -0600

    Merge branch 'new-branch'
    # Conflicts:
    #	test.js

--- test.js
+++ test.js
@@ -1,13 +1,5 @@
 let a = function() {
 	console.log('HELLO WORLD 2!');
-	console.log('something new');
 }
 
 a();
-
-console.log('some text');
-
-let test = 123424;
-console.log(test);
-
-console.log('MORE CHANGES');
